5875a3bdd190fd93f806ce487f896a60e51d0784
a99os <abbosashirboev@gmail.com>
2023-06-02 22:53:06 +0500

    add name and surname

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Body, Controller, Get, Post } from "@nestjs/common";
+import { AppService } from "./app.service";
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Post()
-  async sendMessage(@Body() data:{message:string}){
-    return this.appService.sendMessage(data.message)
+  async sendMessage(
+    @Body() data: { name: string; surname: string; phone_number: string }
+  ) {
+    return this.appService.sendMessage(data);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,18 +1,26 @@
-import { Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/sequelize';
-import { InjectBot } from 'nestjs-telegraf';
-import { Context, Markup, Telegraf } from 'telegraf';
-import { MyBotName } from './app.constants';
-
+import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/sequelize";
+import { InjectBot } from "nestjs-telegraf";
+import { Context, Markup, Telegraf } from "telegraf";
+import { MyBotName } from "./app.constants";
 
 @Injectable()
 export class AppService {
-  constructor(
-    @InjectBot(MyBotName) private readonly bot: Telegraf<Context>,
-  ) {}
-  
-  async sendMessage(msg:string){
-    await this.bot.telegram.sendChatAction(+process.env.CHANNEL_ID,"typing")
-    return await this.bot.telegram.sendMessage(+process.env.CHANNEL_ID,msg)
+  constructor(@InjectBot(MyBotName) private readonly bot: Telegraf<Context>) {}
+
+  async sendMessage(data: {
+    name: string;
+    surname: string;
+    phone_number: string;
+  }) {
+    await this.bot.telegram.sendChatAction(+process.env.CHANNEL_ID, "typing");
+    return await this.bot.telegram.sendMessage(
+      +process.env.CHANNEL_ID,
+      `
+name: ${data.name}
+surname: ${data.surname}
+phone_number: ${data.phone_number}
+    `
+    );
   }
 }
